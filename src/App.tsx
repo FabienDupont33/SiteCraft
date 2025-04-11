4b2628ad58ff0e01c45b707ff7a64277391faee1
BruteEpaisse <fabien.dupont33@gmail.com>
2025-04-11 22:06:27 +0200

    Initial commit - création du projet React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur mon site React avec Material UI</h1>
+      <Button variant="contained" color="primary">
+        Cliquez ici
+      </Button>
+    </div>
+  );
+}
+
+export default App;
