677c63bf0b572bd562102f78d459184eb88ac582
BruteEpaisse <fabien.dupont33@gmail.com>
2025-04-12 13:21:32 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/SiteCraft/', // Remplace "SiteCraft" par le nom de ton dépôt GitHub
+  build: {
+    outDir: 'dist', // Assure-toi que le dossier de sortie est bien 'dist'
+  },
+});
